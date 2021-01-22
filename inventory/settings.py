478c98e6592a854d241ac8a56db2113ee185b3d9
“Varunps91” <Starklabsindia@gmail.com>
2021-01-22 15:49:44 +0530

    first commit

--- inventory/settings.py
+++ inventory/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -122,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
